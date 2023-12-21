097ad7f48498bfd2cae21293bf69f38f15fbe357
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2023-12-21 15:48:09 +0100

    chore: dodaj biblioteki do projektów testowych
    Do każdego z projektów dodano następujące biblioteki:
    - Analizatory xUnit
    - FluentAsserations (oraz analizatory)
    - NSubstitute (oraz analizatory)
    - AutoFixture (z rozszerzeniami integrującymi z xUnit 2 i AutoFixture)
    
    Dodatkowo, dla projektu `DietPenguin.Gui.Tests`:
    - bUnit

--- test/DietPenguin.Gui.Tests/Usings.cs
+++ test/DietPenguin.Gui.Tests/Usings.cs
@@ -1,2 +1,8 @@
 global using Xunit;
+global using FluentAssertions;
+global using NSubstitute;
+global using AutoFixture;
+global using AutoFixture.Xunit2;
+global using AutoFixture.AutoNSubstitute;         
+
 global using DietPenguin.Gui;
