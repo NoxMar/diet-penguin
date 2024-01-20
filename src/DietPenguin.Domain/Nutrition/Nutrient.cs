5de4c9dc335e18d8ee65955b20f39f71fc832cc6
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2024-01-20 21:36:05 +0100

    fix: value-based equality semantics for `Unit` instances
    Due to the fact that `Unit` instances contain `BaseUnit` field base
    equality (and `ToString`) implementations provided by the record
    were resulting in infinite recursion. This was caused by the fact, that
    that base units are their own base units.

--- src/DietPenguin.Domain/Nutrition/Nutrient.cs
+++ src/DietPenguin.Domain/Nutrition/Nutrient.cs
@@ -1,6 +1,17 @@
 namespace DietPenguin.Domain.Nutrition;
 
-public record Nutrient(string Name, Unit PreferredUnit)
+public record Nutrient(string Name, Unit PreferredUnit, decimal EnergyPerPreferred = 0)
 {
-    public static readonly Nutrient Energy = new("Energy", Unit.Kcal);
+    public static readonly Nutrient Energy = new("Energy", Unit.Kcal, 1);
+    public static readonly Nutrient Protein = new("Protein", Unit.Gram, 4);
+    public static readonly Nutrient Carbs = new("Carbohydrates", Unit.Gram, 4);
+    public static readonly Nutrient Fat = new ("Fat", Unit.Gram, 9);
+    public static readonly Nutrient Fiber = new("Fiber", Unit.Gram);
+    public static readonly Nutrient VitC = new("Vitamin C", Unit.Milligram);
+    public static readonly Nutrient VitD = new("Vitamin D", Unit.Microgram);
+    public static readonly Nutrient VitA = new("Vitamin A", Unit.Milligram);
+    public static readonly Nutrient Iron = new("Iron", Unit.Milligram);
+    public static readonly Nutrient Calcium = new("Calcium", Unit.Milligram);
+    public static readonly Nutrient Potassium = new("Potassium", Unit.Milligram);
+    public static readonly Nutrient Water = new("Water", Unit.Milliliter);
 }
