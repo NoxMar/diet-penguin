4142e9554bd6ae7e6dbaab130b1feff34251a389
Marcin "NoxMar" Sladewski <mar4sladewski@gmail.com>
2024-01-22 11:27:23 +0100

    feat: przetłumacz nazwy składników odżywczych

--- src/DietPenguin.Domain/Nutrition/Nutrient.cs
+++ src/DietPenguin.Domain/Nutrition/Nutrient.cs
@@ -2,16 +2,16 @@ namespace DietPenguin.Domain.Nutrition;
 
 public record Nutrient(string Name, Unit PreferredUnit, decimal EnergyPerPreferred = 0)
 {
-    public static readonly Nutrient Energy = new("Energy", Unit.Kcal, 1);
-    public static readonly Nutrient Protein = new("Protein", Unit.Gram, 4);
-    public static readonly Nutrient Carbs = new("Carbohydrates", Unit.Gram, 4);
-    public static readonly Nutrient Fat = new ("Fat", Unit.Gram, 9);
-    public static readonly Nutrient Fiber = new("Fiber", Unit.Gram);
-    public static readonly Nutrient VitC = new("Vitamin C", Unit.Milligram);
-    public static readonly Nutrient VitD = new("Vitamin D", Unit.Microgram);
-    public static readonly Nutrient VitA = new("Vitamin A", Unit.Milligram);
-    public static readonly Nutrient Iron = new("Iron", Unit.Milligram);
-    public static readonly Nutrient Calcium = new("Calcium", Unit.Milligram);
-    public static readonly Nutrient Potassium = new("Potassium", Unit.Milligram);
-    public static readonly Nutrient Water = new("Water", Unit.Milliliter);
+    public static readonly Nutrient Energy = new("Energia", Unit.Kcal);
+    public static readonly Nutrient Protein = new("Białko", Unit.Gram, 4);
+    public static readonly Nutrient Carbs = new("Węglowodany", Unit.Gram, 4);
+    public static readonly Nutrient Fat = new ("Tłuszcz", Unit.Gram, 9);
+    public static readonly Nutrient Fiber = new("Błonnik", Unit.Gram);
+    public static readonly Nutrient VitC = new("Witamina C", Unit.Milligram);
+    public static readonly Nutrient VitD = new("Witamina D", Unit.Microgram);
+    public static readonly Nutrient VitA = new("Witamina A", Unit.Milligram);
+    public static readonly Nutrient Iron = new("Żelazo", Unit.Milligram);
+    public static readonly Nutrient Calcium = new("Wapń", Unit.Milligram);
+    public static readonly Nutrient Potassium = new("Potas", Unit.Milligram);
+    public static readonly Nutrient Water = new("Woda", Unit.Milliliter);
 }
